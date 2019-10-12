b266b72d810ed810bfafe019c6d305198480009e
NUlliiON <atomnicklol@gmail.com>
2019-10-12 23:02:15 +0300

    Remove temp code

--- Program.cs
+++ Program.cs
@@ -28,100 +28,17 @@ namespace Blazor.CssBundler
 
         static async Task Main(string[] args)
         {            
-            //ISettings settings = JsonConvert.DeserializeObject<ISettings>(File.ReadAllText(Path.Combine("settings", "BlazorApp.application.settings.json")));
-
-            int q = Parser.Default.ParseArguments<WatchOptions, ChangeSettingsOptions>(args)
-                .MapResult(
-                  (ChangeSettingsOptions opts) => RunChangeSettingsAndReturnExitCode(opts),
-                  (WatchOptions opts) => RunWatchAndReturnExitCode(opts),
-                  errs => 1);
-
-            
-
             if (args.Length < 1)
             {
                 return;
             }
             try
             {
-                //ExtendedLogger logger = new ExtendedLogger();
-                //CommandRunner cmdRunner = new CommandRunner(logger);
-
-                //ArgumentParser argParser = new ArgumentParser();
-                //Argument[] argObjects = argParser.Parse(string.Join("", args)).ToArray();
-                //var commands = new List<ICommand>()
-                //{
-                //    new ApplicationWatchCommand(),
-                //    new ComponentWatchCommand()
-                //};
-
-                //foreach (ICommand cmd in commands)
-                //{
-                //    if (cmdRunner.OnlyPossibleArguments(argObjects, cmd))
-                //    {
-                //        await cmd.StartAsync(argObjects);
-                //    }
-                //}
-
-                //string[] argNames = argObjects.Select(x => x.Name).ToArray();
-
-                //if (argNames.Contains("watch"))
-                //{
-
-                //}
-
-
-                //foreach (Argument arg in argObjects)
-                //{
-                //    if (arg.Name == "change-settings")
-                //    {
-
-                //        if (arg.HasFirstValue)
-                //        {
-                //            await cmdRunner.ChangeSettings();
-                //        }
-                //        else
-                //        {
-
-                //        }
-                //    }
-                //    else if (arg.Name == "settings-list")
-                //    {
-                //        cmdRunner.SettingsList();
-                //    }
-                //    else if (arg.Name == "watch-application")
-                //    {
-                //        if (arg.HasFirstValue && arg.HasSecondValue)
-                //        {
-                //            var settings = await SettingsManager.ReadAsync<ApplicationSettings>(arg.FirstValue, arg.SecondValue);
-                //            await cmdRunner.AppWatch(settings);
-                //        }
-                //        else
-                //        {
-                //            cmdRunner.AppWatch();
-                //        }
-                //    }
-                //    else
-                //    {
-                //        if (args[0] == "watch-app")
-                //        {
-
-                //        }
-                //        else if (args[0] == "watch-component")
-                //        {
-                //            //ComponentSettings settings = await SettingsManager.ReadAsync<ComponentSettings>();
-                //            //await cmdRunner.ComponentWatch(settings);
-                //        }
-                //        else if (args[0] == "build-app")
-                //        {
-
-                //        }
-                //        else if (args[0] == "build-component")
-                //        {
-
-                //        }
-                //    }
-                //}
+                Parser.Default.ParseArguments<WatchOptions, ChangeSettingsOptions>(args)
+                    .MapResult(
+                    (ChangeSettingsOptions opts) => RunChangeSettingsAndReturnExitCode(opts),
+                    (WatchOptions opts) => RunWatchAndReturnExitCode(opts),
+                    errs => 1);
             }
             catch (InvalidSettingsException)
             {
