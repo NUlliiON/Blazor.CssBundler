85e5e69c94751ae8d7c78a772c4c4e779022680b
NUlliiON <atomnicklol@gmail.com>
2019-10-13 00:09:47 +0300

    Create base class for commands

---
+++ Commands/BaseCommand.cs
@@ -0,0 +1,21 @@
+﻿using Blazor.CssBundler.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blazor.CssBundler.Commands
+{
+    abstract class BaseCommand<T>
+    {
+        protected ILogger Logger;
+
+        public BaseCommand(ILogger logger)
+        {
+            Logger = logger;
+        }
+
+        public abstract void Execute(T options);
+        public abstract Task ExecuteAsync(T options);
+    }
+}
