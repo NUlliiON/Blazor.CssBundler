53a719c833798175561553522793b222b7cf794d
NUlliiON <atomnicklol@gmail.com>
2019-10-13 23:11:24 +0300

    Add fluent extension: Set

---
+++ Extensions/FluentExtension.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Blazor.CssBundler.Extensions
+{
+    static class FluentExtension
+    {
+        /// <summary>
+        /// Fluent helper for easy set property values in object
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="obj"></param>
+        /// <param name="act"></param>
+        public static void Set<T>(this T obj, Action<T> act)
+        {
+            act.Invoke(obj);
+        }
+    }
+}
