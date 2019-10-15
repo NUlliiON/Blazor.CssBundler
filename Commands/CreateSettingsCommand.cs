d9e98fbf924a96c43b340541af0bf33c8169a3ce
Eugene Antonov <atomnicklol@gmail.com>
2019-10-15 22:13:29 +0300

    Create class with template for another command

---
+++ Commands/CreateSettingsCommand.cs
@@ -0,0 +1,22 @@
+﻿using Blazor.CssBundler.Commands.Options;
+using Blazor.CssBundler.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blazor.CssBundler.Commands
+{
+    class CreateSettingsCommand : BaseCommand<CreateSettingsOptions>
+    {
+        public override void Execute(ILogger logger, CreateSettingsOptions options)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Task ExecuteAsync(ILogger logger, CreateSettingsOptions options)
+        {
+            
+        }
+    }
+}
