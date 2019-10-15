7457d7a7edf9447f803d80825b1e19a5d5cdebcc
Eugene Antonov <atomnicklol@gmail.com>
2019-10-15 19:07:59 +0300

    Remove dash char from verb

--- Commands/Options/ChangeSettingsOptions.cs
+++ Commands/Options/ChangeSettingsOptions.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace Blazor.CssBundler.Commands.Options
 {
-    [Verb("-change-settings", HelpText = "Change settings")]
+    [Verb("change-settings", HelpText = "Change settings")]
     class ChangeSettingsOptions
     {
         [Option(SetName = "name", HelpText = "Settings name")]
