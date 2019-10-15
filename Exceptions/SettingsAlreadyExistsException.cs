a087a765e886b35b85be64dc0f639c6ef3b3251f
Eugene Antonov <atomnicklol@gmail.com>
2019-10-15 19:40:49 +0300

    Create SettingsAlreadyExistsException

---
+++ Exceptions/SettingsAlreadyExistsException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blazor.CssBundler.Exceptions
+{
+    class SettingsAlreadyExistsException : Exception
+    {
+        /// <summary>
+        /// Settings name
+        /// </summary>
+        public string SettingsName { get; set; }
+
+        public SettingsAlreadyExistsException(string settingsName)
+            : base()
+        {
+            SettingsName = settingsName;
+        }
+    }
+}
