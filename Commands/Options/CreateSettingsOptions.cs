e3c949fe19d78fc62d0347d2914bc07a8919241a
Eugene Antonov <atomnicklol@gmail.com>
2019-10-15 22:11:08 +0300

    Create options for command

---
+++ Commands/Options/CreateSettingsOptions.cs
@@ -0,0 +1,18 @@
+﻿using Blazor.CssBundler.Models.Settings;
+using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blazor.CssBundler.Commands.Options
+{
+    [Verb("create-settings", HelpText = "Create new settings")]
+    class CreateSettingsOptions
+    {
+        [Option(SetName = "name", HelpText = "Settings name", Required = true)]
+        public string SettingsName { get; set; }
+
+        [Option(SetName = "type", HelpText = "Settings type", Required = true)]
+        public SettingsType SettingsType { get; set; }
+    }
+}
