7b22ac71ca0c4b6ef077a875bcedbdbeb04beaf3
Eugene Antonov <atomnicklol@gmail.com>
2019-10-15 21:39:22 +0300

    Create SettingsListOptions class for SettingsListCommand class

---
+++ Commands/Options/SettingsListOptions.cs
@@ -0,0 +1,11 @@
+﻿using CommandLine;
+
+namespace Blazor.CssBundler.Commands.Options
+{
+    [Verb("settings-list", HelpText = "Display settings list")]
+    class SettingsListOptions
+    {
+        [Option(SetName = "type", Default = SettingsDisplayType.All, HelpText = "Settings display type")]
+        public SettingsDisplayType Type { get; set; }
+    }
+}
