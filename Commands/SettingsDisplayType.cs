c695bcd57b91694549cfa35f9626b43a7c131852
Eugene Antonov <atomnicklol@gmail.com>
2019-10-15 21:47:12 +0300

    Move enum to another directory

--- Commands/SettingsDisplayType.cs
+++ Commands/SettingsDisplayType.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Blazor.CssBundler.Commands.Options
+namespace Blazor.CssBundler.Commands
 {
     public enum SettingsDisplayType
     {
