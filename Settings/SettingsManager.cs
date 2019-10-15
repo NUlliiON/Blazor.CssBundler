b98cbbc03537c4415ce8aeaf34a7e301c46e4f98
NUlliiON <atomnicklol@gmail.com>
2019-10-15 14:36:26 +0300

    Change logic of MakeSettingsPath method + some changes in ReadJsonAsync method

--- Settings/SettingsManager.cs
+++ Settings/SettingsManager.cs
@@ -4,6 +4,7 @@ using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,6 +12,8 @@ namespace Blazor.CssBundler.Settings
 {
     public class SettingsManager
     {
+        private static readonly string _settingsPath = Path.Combine(Environment.CurrentDirectory, "settings");
+
         /// <summary>
         /// Save settings asynchronously
         /// </summary>
@@ -24,10 +27,10 @@ namespace Blazor.CssBundler.Settings
                 throw new ArgumentNullException("settings");
             }
 
-            byte[] jsonBytes = Encoding.Default.GetBytes(JsonConvert.SerializeObject(settings));
-            using (FileStream fs = new FileStream(MakeSettingsFileName(name), FileMode.OpenOrCreate))
+            byte[] buffer = Encoding.Default.GetBytes(JsonConvert.SerializeObject(settings));
+            using (FileStream fs = new FileStream(MakeSettingsPath(name), FileMode.OpenOrCreate))
             {
-                await fs.WriteAsync(jsonBytes, 0, jsonBytes.Length);
+                await fs.WriteAsync(buffer, 0, buffer.Length);
             }
         }
 
@@ -35,15 +38,18 @@ namespace Blazor.CssBundler.Settings
         /// Read settings from file asynchronously
         /// </summary>
         /// <typeparam name="T"></typeparam>
-        /// <param name="filePath">full path to settings</param>
+        /// <param name="name">settings name</param>
         /// <returns></returns>
-        public static async Task<T> ReadJsonAsync<T>(string filePath) where T : BaseSettings
+        public static async Task<T> ReadJsonAsync<T>(string name) where T : BaseSettings
         {
             try
             {
-                
+                string settingsPath = await GetAboutAllSettings()
+                    .Where(x => x.name == name)
+                    .Select(x => x.path).FirstOrDefaultAsync();
+
                 byte[] buffer;
-                using (FileStream fs = new FileStream(filePath, FileMode.OpenOrCreate))
+                using (FileStream fs = new FileStream(settingsPath, FileMode.OpenOrCreate))
                 {
                     buffer = new byte[fs.Length];
                     await fs.ReadAsync(buffer, 0, buffer.Length);
@@ -67,20 +73,20 @@ namespace Blazor.CssBundler.Settings
         /// </summary>
         /// <param name="name">settings name</param>
         /// <returns></returns>
-        public static bool Has(string name) => File.Exists(Path.Combine("settings", MakeSettingsFileName(name)));
+        public static bool Has(string name) => File.Exists(MakeSettingsPath(name));
 
         /// <summary>
         /// Get all settings
         /// </summary>
         /// <returns></returns>
-        public static async IAsyncEnumerable<(string name, SettingsType type, DateTime lastChangingTime)> GetAboutAllSettings()
+        public static async IAsyncEnumerable<(string name, string path, SettingsType type, DateTime lastChangingTime)> GetAboutAllSettings()
         {
-            foreach (FileInfo file in new DirectoryInfo("settings").GetFiles("*.settings.json", SearchOption.TopDirectoryOnly))
+            foreach (FileInfo file in new DirectoryInfo("settings").GetFiles("*.json", SearchOption.TopDirectoryOnly))
             {
-                BaseSettings settings = await ReadJsonAsync<BaseSettings>(file.FullName);
+                BaseSettings settings = await ReadJsonAsync<BaseSettings>(Path.GetFileNameWithoutExtension(file.Name));
                 if (settings?.Name != null)
                 {
-                    yield return (settings.Name, settings.Type, file.LastWriteTime);
+                    yield return (settings.Name, file.FullName, settings.Type, file.LastWriteTime);
                 }
             }
         }
@@ -90,16 +96,14 @@ namespace Blazor.CssBundler.Settings
         /// </summary>
         /// <param name="settingsTypeEnum">settings type</param>
         ///// <returns></returns>
-        public static async IAsyncEnumerable<(string name, SettingsType type, DateTime lastChaningTime)> GetAboutSettings(SettingsType settingsTypeEnum)
+        public static async IAsyncEnumerable<(string name, string path, SettingsType type, DateTime lastChaningTime)> GetAboutSettings(SettingsType settingsTypeEnum)
         {
-            foreach (FileInfo file in new DirectoryInfo("settings").GetFiles($"*.settings.json", SearchOption.TopDirectoryOnly))
+            foreach (FileInfo file in new DirectoryInfo("settings").GetFiles($"*.json", SearchOption.TopDirectoryOnly))
             {
-                BaseSettings settings = await ReadJsonAsync<BaseSettings>(file.FullName);
-                if (settings?.Type == settingsTypeEnum && 
-                    settings?.Name != null
-                    )
+                BaseSettings settings = await ReadJsonAsync<BaseSettings>(Path.GetFileNameWithoutExtension(file.Name));
+                if (settings?.Name != null && settings?.Type == settingsTypeEnum)
                 {
-                    yield return (settings.Name, settings.Type, file.LastWriteTime);
+                    yield return (settings.Name, file.FullName, settings.Type, file.LastWriteTime);
                 }
             }
         }
@@ -108,11 +112,10 @@ namespace Blazor.CssBundler.Settings
         /// Make settings path
         /// </summary>
         /// <param name="name">settings name</param>
-        /// <param name="type">settings type</param>
         /// <returns></returns>
-        private static string MakeSettingsFileName(string name)
+        private static string MakeSettingsPath(string name)
         {
-            return $"{name}.settings.json";
+            return Path.Combine(_settingsPath, $"{name}.settings.json");
         }
     }
 }
