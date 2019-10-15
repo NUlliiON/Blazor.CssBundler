2527b1002b97a4cd9b80a134c3ff587bec90e932
Eugene Antonov <atomnicklol@gmail.com>
2019-10-15 21:40:25 +0300

    Create SettingsDisplayType enum

---
+++ Commands/Options/SettingsDisplayType.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blazor.CssBundler.Commands.Options
+{
+    public enum SettingsDisplayType
+    {
+        /// <summary>
+        /// Display all settings
+        /// </summary>
+        All = 0,
+        /// <summary>
+        /// Display only application type
+        /// </summary>
+        Application,
+        /// <summary>
+        /// Display only component type
+        /// </summary>
+        Component,
+    }
+}
