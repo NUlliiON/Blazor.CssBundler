1f035317679f4f0fa2a4bd02087e0a4fd3323784
Eugene Antonov <atomnicklol@gmail.com>
2019-10-19 14:50:19 +0300

    Create JsonWriter for simplify json writing

---
+++ Writers/IWriter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blazor.CssBundler.Writers
+{
+    interface IWriter
+    {
+        void Write<T>(string filePath, T data);
+        Task WriteAsync<T>(string filePath, T data);
+    }
+}
---
+++ Writers/JsonWriter.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blazor.CssBundler.Writers
+{
+    class JsonWriter : IWriter
+    {
+        public void Write<T>(string filePath, T data) 
+        {
+            string json = JsonConvert.SerializeObject(data, Formatting.Indented);
+            File.WriteAllText(filePath, json);
+        }
+
+        public async Task WriteAsync<T>(string filePath, T data)
+        {
+            string json = JsonConvert.SerializeObject(data, Formatting.Indented);
+            await File.WriteAllTextAsync(filePath, json);
+        }
+    }
+}
